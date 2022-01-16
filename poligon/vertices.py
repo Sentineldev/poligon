281ae69c280bcec00abe1843cba32ba26c2d0be0
Jesus Figuera <jesusfiguera20@gmail.com>
2022-01-16 18:01:59 -0400

    Rotacion de la figura, ajuste hecho por Cesar Zabala

--- poligon/vertices.py
+++ poligon/vertices.py
@@ -5,8 +5,8 @@ def definir_vertices(lados):
     centro = (210,200) #punto central del poligono
     vertices = [] #lista para guardar las coordenadas
     for i in range(lados):
-        x = round(radio*math.cos(math.pi/180*grados)+centro[0]) #se calcula la posicion x en la pantalla sumandole el centro
-        y = round(radio*math.sin(math.pi/180*grados)+centro[1])# secalcula la posicion de y en la pantalla sumandole el centro
+        x = round(radio*math.cos(math.pi/180*grados-math.pi/2)+centro[0]) #se calcula la posicion x en la pantalla sumandole el centro
+        y = round(radio*math.sin(math.pi/180*grados-math.pi/2)+centro[1])# secalcula la posicion de y en la pantalla sumandole el centro
         grados+=360/lados #y los grados para la separacion que tendran los vertices unos de otros
         vertices.append([])
         vertices[i].append(x) #se añaden en lista
--- poligon/vertices_indices.py
+++ poligon/vertices_indices.py
@@ -6,8 +6,8 @@ def definir_vertices_indices(lados):
     centro = (210,200) #el mismo procedimiento para conseguir los vertices, solo que añadiendole identificador mediante un diccionario
     vertices = []
     for i in range(lados):
-        x = round(radio*math.cos(math.pi/180*grados)+centro[0])
-        y = round(radio*math.sin(math.pi/180*grados)+centro[1])
+        x = round(radio*math.cos(math.pi/180*grados-math.pi/2)+centro[0])
+        y = round(radio*math.sin(math.pi/180*grados-math.pi/2)+centro[1])
         grados+=360/lados
         vertices.append({
             "punto":str(i),
